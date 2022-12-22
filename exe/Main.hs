bf490f7f6c4e42e1ad3d1560d4cc79c992e76058
Eric Conlon <ejconlon@gmail.com>
2022-12-21 19:47:00 -0800

    init

--- exe/Main.hs
+++
@@ -1,6 +0,0 @@
-module Main (main) where
-
-import Lyric (exe)
-
-main :: IO ()
-main = exe
--- src/Lyric.hs
+++ src/Lyric.hs
@@ -1,4 +1,55 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Lyric where
 
-exe :: IO ()
-exe = putStrLn "hello, world"
+import Control.Exception (Exception)
+import Control.Monad.Except (ExceptT, MonadError, runExceptT)
+import Control.Monad.State.Strict (MonadState, State, runState)
+import Data.Functor.Foldable.TH (makeBaseFunctor)
+import Data.Sequence (Seq (..))
+import Data.String (IsString)
+import Data.Text (Text)
+
+newtype TmVar = TmVar { unTmVar :: Text }
+  deriving stock (Show)
+  deriving newtype (Eq, Ord, IsString)
+
+data Op = OpGt | OpAdd
+  deriving stock (Eq, Ord, Show, Enum, Bounded)
+
+data Exp =
+    ExpTup !(Seq Exp)
+  | ExpLam !TmVar Exp
+  | ExpInt !Int
+  | ExpOp !Op
+  | ExpApp Exp Exp
+  | ExpSeq Exp Exp
+  | ExpExists !TmVar Exp
+  | ExpFail
+  | ExpAlt Exp Exp
+  | ExpOne Exp
+  | ExpAll Exp
+  deriving stock (Eq, Ord, Show)
+
+makeBaseFunctor ''Exp
+deriving instance Eq r => Eq (ExpF r)
+deriving instance Ord r => Ord (ExpF r)
+deriving instance Show r => Show (ExpF r)
+
+data Err = ErrFoo
+  deriving stock (Eq, Ord, Show)
+
+instance Exception Err
+
+data St = St
+  {
+  } deriving stock (Eq, Ord, Show)
+
+newtype M a = M { unM :: ExceptT Err (State St) a }
+  deriving newtype (Functor, Applicative, Monad, MonadError Err, MonadState St)
+
+runM :: M a -> St -> (Either Err a, St)
+runM = runState . runExceptT . unM
+
+step :: M ()
+step = pure ()
